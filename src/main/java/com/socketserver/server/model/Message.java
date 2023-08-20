e8a803cdff58dca553ce55d714fc3793edcab4f2
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-08-20 21:34:00 +0530

    Update Message.java

--- src/main/java/com/socketserver/server/model/Message.java
+++ src/main/java/com/socketserver/server/model/Message.java
@@ -1,12 +1,12 @@
-package com.socketserver.server.model;
+    package com.socketserver.server.model;
 
-import lombok.Data;
+    import lombok.Data;
 
-@Data
-public class Message {
-    private String senderName;
-    private String receiverName;
-    private String message;
-    private Status status;
+    @Data
+    public class Message {
+        private String senderName;
+        private String receiverName;
+        private String messageContent;
+        private Status status;
 
-}
+    }

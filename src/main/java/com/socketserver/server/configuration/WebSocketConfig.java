e5e9ab9e17e58677babdfbdc1e84d2f7365acf2f
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-08-20 21:31:59 +0530

    Update WebSocketConfig.java

--- src/main/java/com/socketserver/server/configuration/WebSocketConfig.java
+++ src/main/java/com/socketserver/server/configuration/WebSocketConfig.java
@@ -12,13 +12,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom,/user");
-        registry.setUserDestinationPrefix("/user");
+        registry.enableSimpleBroker("/chatroom");
+
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*")
+
+                .withSockJS();
     }
 }
